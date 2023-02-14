38c7aa346d7dcf2b97a27aaf98631a95a910c04d
Blookers <blookyl305@gmail.com>
2023-02-15 02:58:56 +1000

    🦑

--- src/commands/general/test-info.ts
+++ src/commands/general/test-info.ts
@@ -1,3 +1,6 @@
 import { text } from '../response.ts';
 import { command } from '../mod.ts';
-export default command(({ locale }) => text(`locale: ${locale}`));
+export default command(({ locale }) => {
+	console.log(Deno.env.toObject());
+	return text(`locale: ${locale}\ndeployment: ${Deno.env.get('DENO_DEPLOYMENT_ID')}`);
+});
